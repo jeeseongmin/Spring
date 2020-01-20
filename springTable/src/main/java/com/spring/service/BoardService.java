67b0422d6ce1a69d7f8f4831d04625e14e0d7ca1
21500706 <21500706@handong.edu>
2020-01-21 02:21:51 +0900

    2020-01-21 homework

---
+++ springTable/src/main/java/com/spring/service/BoardService.java
@@ -0,0 +1,12 @@
+package com.spring.service;
+import java.util.List;
+
+import com.spring.domain.BoardVO;
+public interface BoardService {
+	  public void regist(BoardVO board) throws Exception;
+	  public BoardVO read(Integer bno) throws Exception;
+	  public void modify(BoardVO board) throws Exception;
+	  public void remove(Integer bno) throws Exception;
+	  public List<BoardVO> listAll() throws Exception;
+}
+
